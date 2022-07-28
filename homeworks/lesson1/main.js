a11d2c110e86d01deff690158f92f64f6bcc7ce9
OlyaSergienko <olya86@ua.fm>
2022-07-29 00:06:11 +0300

    111

---
+++ homeworks/lesson1/main.js
@@ -0,0 +1,45 @@
+// let name;
+// name = 'hello';
+// console.log(name);
+// document.write(name);
+// alert(name);
+//
+// name = ' owu';
+// console.log(name);
+// document.write(name);
+// alert(name);
+//
+// name = 10;
+// console.log(name);
+// document.write(name);
+// alert(name);
+//
+// name = true;
+// console.log(name);
+// document.write(name);
+// alert(name);
+
+
+let firstName = 'Olga';
+let middleName = 'Sergeevna';
+let lastName = 'Sergienko';
+
+let myName = `${firstName} ${middleName} ${lastName}`;
+console.log(myName);
+document.write(myName);
+
+let a = 100;
+console.log(typeof a);
+let b = '100';
+console.log(typeof b);
+let c = true;
+console.log(typeof c);
+
+let anyth = prompt('name');
+console.log(anyth);
+
+let anyth1 = prompt('middleName');
+console.log(anyth1);
+
+let anyth2 = +prompt('age');
+console.log(anyth2);
