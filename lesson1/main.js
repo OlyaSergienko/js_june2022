0a8d21f810c4cf258caa538b2967c48f1f7762e9
OlyaSergienko <olya86@ua.fm>
2022-07-28 22:24:47 +0300

    Initial commit

---
+++ lesson1/main.js
@@ -0,0 +1,13 @@
+let a;
+a = 100;
+console.log(a);
+console.log(typeof a);
+
+let str1 = 'hello';
+let str2 = 'okten';
+
+let word1 = str1 + str2;
+console.log(word1);
+
+let word2 = `${str1} ${str2}`;
+console.log(word2);
